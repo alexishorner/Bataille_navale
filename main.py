ae7c9e8115afa97995c3ef275c3c653eac4b151e
alexishorner <39777324+alexishorner@users.noreply.github.com>
2018-09-27 21:18:59 +0200

    nouvelle version

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 #Ce fichier est l'endroit où le programme commence.
+import tkinter #Ce module permet de faire une interface graphique (une application avec des fenêtres et pas seulement du texte dans la console)
+fenetre = tkinter.Tk() #Crée une fenêtre
+fenetre.mainloop()
